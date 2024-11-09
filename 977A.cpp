f2715737535ae87c0b0407fa85f7848b14dc2fd1
Pranto Kumar <prantokumar.net@gmail.com>
2024-11-09 13:30:42 +0600

    Wrong Subtractions

---
+++ 977A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define bust ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define cs cout<<endl<<"-------------------------"<<endl;
+#define cn cout<<endl;
+#define pf(x) cout<<x<<endl;
+#define sc(x) cin>>x;
+#define itr(i,vec) for(auto i : vec) cout<<i<<endl;
+#define fr(itr,size) for(int i = 0; i < size; i++) cout<<i<<" "<<endl;
+#define wh(com1,com2) while(com1 != com2) cout<<com1<<" "<<endl;
+int main(){
+bust;
+ll n; short k;
+cin>>n>>k;
+while (k--){
+  if( n%10 != 0 ) n -= 1;
+  else n /= 10;
+}
+cout<<n<<endl;
+return 0;
+}
